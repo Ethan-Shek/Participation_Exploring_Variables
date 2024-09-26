931f6815b16761ed42be3d7b63196609464bad45
Ethan-Shek <dinoeshek@gmail.com>
2024-09-26 08:20:58 -0400

    Name

---
+++ Participation_Exploring_Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Participation_Exploring_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroFirstName = "Ethan";
+            string heroLastName = "Shek";
+            string heroFullName = heroFirstName + " " + heroLastName;
+
+            Console.WriteLine("Behold the glorious hero!");
+            Console.WriteLine(heroFullName);
+        }
+    }
+}
